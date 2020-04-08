5cce6a74af4f5bd50b53ca79bb34d2dc75e1e850
belliamatteo <bellia138@gmail.com>
2020-04-08 13:30:49 +0000

    01.0

---
+++ first-routed-app/src/app/spotify.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+//Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //Ottengo il modulo HttpClient
+  constructor(private http: HttpClient) { }
+
+  searchTrack(query: string) {
+    const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQCVcy7wm28tyMC74FfhcRPtRpNjZfnIk0eFb3lQRPwXwlBBey4FBAcHq6OXy3kSTXOa3zf_xafgFxKIZtB7irOdv0hxFNP8nyqt7stkkGBZoLooK1nwMBUxeL0gyAqMl2s46CM9bkPWpZM'
+    });
+
+    let obsTracks = this.http.get(url, { headers });
+    return obsTracks;
+ //Ritorno un observable ai componenti che richiedono il servizio
+  }
+}
