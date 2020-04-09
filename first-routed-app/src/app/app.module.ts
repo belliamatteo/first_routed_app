1c27573a26037d337a6e1101f3647766fbe5c19f
belliamatteo <bellia138@gmail.com>
2020-04-09 14:46:32 +0000

    01.1

--- first-routed-app/src/app/app.module.ts
+++ first-routed-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
+import { SearchComponent } from './search/search.component';
+import { TrackComponent } from './track/track.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    AlbumComponent,
+    ArtistComponent,
+    SearchComponent,
+    TrackComponent
   ],
   imports: [
     BrowserModule,
--- first-routed-app/src/app/spotify.service.ts
+++ first-routed-app/src/app/spotify.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
-   //url per oauth: https://developer.spotify.com/console/get-search-item/
+  //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
   constructor(private http: HttpClient) { }
 
@@ -19,6 +21,16 @@ export class SpotifyService {
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
- //Ritorno un observable ai componenti che richiedono il servizio
+    //Ritorno un observable ai componenti che richiedono il servizio
   }
+
+  getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization: environment.oauthToken
+    });
+    return this.http.get(url, { headers });
+  }
+
+
 }
